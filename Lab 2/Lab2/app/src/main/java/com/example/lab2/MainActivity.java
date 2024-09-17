00c6d7945de6e24e836dfe153596ae85ba1489bb
Khánh Duy <129584304+D3504@users.noreply.github.com>
2024-09-17 08:12:19 +0700

    Lab2

--- Lab 2/Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab 2/Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.lab2;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,23 +22,13 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private int[] colorArr = {R.color.purple_200, R.color.teal_700, R.color.teal_200, R.color.black};
     Random random = new Random();
+    LinearLayout layout;
+    RelativeLayout loading;
 
     private void RandomColor(){
         LinearLayout layout = findViewById(R.id.main);
         int randomColor = random.nextInt(colorArr.length);
         layout.setBackgroundColor(colorArr[randomColor]);
-    }
-
-    private void customPB(){
-        ProgressBar progressBar = findViewById(R.id.ProgressBar);
-        Button button = findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                progressBar.incrementProgressBy(10);
-            }
-        });
     }
 
     @Override
@@ -50,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         RandomColor();
-        customPB();
+        layout = findViewById(R.id.main);
+        loading = findViewById(R.id.layout_loading);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loading.setVisibility(View.GONE);
+            }
+        }, 2000);
     }
 }
